95d41aeab7cf2e5dd8e6268286dbc10524090a7b
AlexzPurewoko <purwoko908@gmail.com>
2023-12-13 11:29:09 +0700

    add RuleTest

---
+++ tests/Unit/Objects/RuleTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use Abilities\Objects\Action;
+use Abilities\Objects\Resource;
+use Abilities\Objects\Rule;
+use Abilities\Objects\Scope;
+
+function makeRule(
+    Resource $resource = new Resource('resource'),
+    bool $isInverted = false
+): Rule {
+    return new Rule(
+        new Scope('scope'),
+        $resource,
+        new Action('action'),
+        $isInverted
+    );
+};
+
+describe("getRuleId function test", function () {
+    it("must throw error when not calling setRuleId beforehand", function () {
+        makeRule()->getRuleId();
+    })->throws(
+        Exception::class,
+        "Don't forget to call 'setRuleId()' before get the rule ID"
+    );
+
+    it("returns expected ruleId", function () {
+        $rule = makeRule();
+        $rule->setRuleId(1);
+
+        expect($rule->getRuleId())->toBeOne();
+    });
+});
+
+describe("toString function test", function () {
+    test("successfully encode inverted rules", function () {
+        $rule = makeRule(isInverted: true);
+        expect("$rule")->toBeString("!scope:resource:action");
+    });
+    test("successfully encode normal rules", function () {
+        $rule = makeRule();
+        expect("$rule")->toBeString("scope:resource:action");
+    });
+    test("successfully rule with custom resource", function () {
+        $rule = makeRule(
+            resource: new Resource(
+                'res',
+                [
+                    'param_1' => 22
+                ]
+            )
+        );
+        expect("$rule")->toBeString("scope:res/{param_1:22}:action");
+    });
+});
