6688d6e0b61ee4e79989f6ce8e86b6b14e36f708
AlexzPurewoko <purwoko908@gmail.com>
2023-12-13 10:48:22 +0700

    add rule object

---
+++ src/Abilities/Objects/Rule.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Abilities\Objects;
+
+use Exception;
+
+class Rule
+{
+    private ?int $ruleId = null;
+
+    public function __construct(
+        private readonly Scope $scope,
+        private readonly Resource $resource,
+        private readonly Action $action,
+        private readonly bool $isInverted = false
+    ) {
+    }
+
+    public function setRuleId(int $id): void
+    {
+        $this->ruleId = $id;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getRuleId(): int
+    {
+        if ($this->ruleId === null){
+            throw new Exception("Don't forget to call 'setRuleId()' before get the rule ID");
+        }
+
+        return $this->ruleId;
+    }
+
+    public function getScope(): Scope
+    {
+        return $this->scope;
+    }
+
+    public function getResource(): Resource
+    {
+        return $this->resource;
+    }
+
+    public function getAction(): Action
+    {
+        return $this->action;
+    }
+
+    public function isInverted(): bool
+    {
+        return $this->isInverted;
+    }
+
+    public function __toString(): string
+    {
+        return ($this->isInverted() ? '!' : '') . ':' . $this->getScope() . ':' . $this->getResource() . ':' . $this->getAction();
+    }
+}
