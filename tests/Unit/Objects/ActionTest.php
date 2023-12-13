43b33d1f8ae2637507e377970f126e2072df7ec9
AlexzPurewoko <purwoko908@gmail.com>
2023-12-14 05:17:06 +0700

    add test it must fai when assigned with empty value

--- tests/Unit/Objects/ActionTest.php
+++ tests/Unit/Objects/ActionTest.php
@@ -2,6 +2,13 @@
 
 use Abilities\Objects\Action;
 
+it('must fail when assigned with empty value', function () {
+    new Action('');
+})->throws(
+    InvalidArgumentException::class,
+    'Action must not be empty'
+);
+
 test("Must use star symbol when using default argument", function () {
     expect((new Action())->get())->toBeString('*');
 });
