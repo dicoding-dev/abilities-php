8af300b0c90867d7c90b1657a8e2345c802e017f
AlexzPurewoko <purwoko908@gmail.com>
2023-12-14 09:05:58 +0700

    add precedence check for matchField()

--- src/Abilities/Objects/Resource.php
+++ src/Abilities/Objects/Resource.php
@@ -35,6 +35,45 @@ class Resource
         return $this->getField() === '*';
     }
 
+    public function matchField(mixed $field): bool
+    {
+        if ($this->allField()) {
+            return true;
+        }
+
+        if (empty($field)) {
+            return false;
+        }
+
+        if ($this->isIntOrStringField($this->field) && $this->isIntOrStringField($field)) {
+            return "" . $this->field === "$field";
+        }
+
+        if (is_array($this->field)) {
+            if (is_array($field)) {
+                foreach ($field as $fieldItem) {
+                    if(!in_array($fieldItem, $this->field)) {
+                        return false;
+                    }
+                }
+                return true;
+            } else {
+                return in_array($field, $this->field);
+            }
+        }
+
+        if (is_object($this->field) && is_object($field)) {
+            return $field == $this->field;
+        }
+
+        return false;
+    }
+
+    private function isIntOrStringField(mixed $field): bool
+    {
+        return is_string($field) || is_int($field);
+    }
+
     public function __toString(): string
     {
         if (empty($this->getField())) {
--- tests/Unit/Objects/ResourceTest.php
+++ tests/Unit/Objects/ResourceTest.php
@@ -54,3 +54,64 @@ describe("allField() function test", function () {
         )->toBeFalse();
     });
 });
+
+
+/**
+ * matchField() Precedence
+ *  1. Star-<field> rules or Empty-<field> rules         --> jobs:vacancies/*:update or jobs:vacancies:update
+ *  2. Single-<field> rules                              --> jobs:vacancies/2:update
+ *  3. Array-<field> rules (OR method)                   --> jobs:vacancies/[4, 5, 6]:update
+ *  4. Object-<field> rules (AND method per attributes)  --> jobs:vacancies/{"authoredBy": 22}:update
+ */
+
+describe('matchField() function test', function () {
+    it('must return true if the resource rule has star field', function () {
+        expect(
+            (new Resource('some_resource'))->matchField('some_field')
+        )->toBeTrue();
+    });
+
+    it('must return false if the argument is empty', function () {
+        expect(
+            (new Resource('some_resource', 'some_field'))->matchField(null)
+        )->toBeFalse();
+    });
+
+    it('must return true if match the single field ', function () {
+        expect(
+            (new Resource('some_resource', 'some_field'))->matchField('some_field')
+        )->toBeTrue();
+    });
+
+    it('must return false if fields does not contains the field argument ', function () {
+        expect(
+            (new Resource('some_resource', [1, 2, 3, 4, 5]))->matchField([4, 10, 3])
+        )->toBeFalse();
+    });
+
+    it('must return true if fields contains the field argument ', function () {
+        expect(
+            (new Resource('some_resource', [1, 2, 3, 4, 5]))->matchField([2, 4])
+        )->toBeTrue();
+    });
+
+    it('must return true if fields contains exactly one field argument ', function () {
+        expect(
+            (new Resource('some_resource', [1, 2, 3, 4, 5]))->matchField(5)
+        )->toBeTrue();
+    });
+
+    it('must return true if field object match exactly with argument', function () {
+        expect(
+            (new Resource('some_resource', (object) ['author' => 'John', 'age' => '5']))
+                ->matchField((object) ['author' => 'John', 'age' => 5])
+        )->toBeTrue();
+    });
+
+    it('must return false if field object doesnt match exactly with argument', function () {
+        expect(
+            (new Resource('some_resource', (object) ['author' => 'John', 'age' => '5']))
+                ->matchField((object) ['author' => 'John', 'age' => 10])
+        )->toBeFalse();
+    });
+});
