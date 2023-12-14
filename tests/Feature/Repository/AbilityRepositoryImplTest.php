2cd77fb02960b6dccbe078adc39f026993419151
AlexzPurewoko <purwoko908@gmail.com>
2023-12-15 06:39:08 +0700

    add the update and delete ability test

--- tests/Feature/Repository/AbilityRepositoryImplTest.php
+++ tests/Feature/Repository/AbilityRepositoryImplTest.php
@@ -1,57 +1,9 @@
 <?php
 
+use Abilities\Core\AbilityChecker;
 use Abilities\Core\AbilityCheckerImpl;
 use Abilities\Repository\AbilityRepositoryImpl;
 use Abilities\Storage\StorageInterface;
-
-class StorageFixture implements StorageInterface
-{
-    private array $rules = [
-        'scope1:resource1:read',
-        'scope1:resource2:update'
-    ];
-
-    /**
-     * @inheritDoc
-     */
-    public function onInsertNewRule(int|string $userIdentification, string $newRule): int
-    {
-        $this->rules[] = $newRule;
-        return count($this->rules);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function onUpdateRule(int $ruleId, string $updatedRule): void
-    {
-        $this->rules[$ruleId - 1] = $updatedRule;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function onDeleteSpecificRule(int $deletedRuleId): void
-    {
-        unset($this->rules[$deletedRuleId]);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function onGetRulesByUserId(int|string $userIdentification): array
-    {
-        $mappedObjectRules = [];
-        foreach ($this->rules as $index=>$rule) {
-            $mappedObjectRules[] = (object) [
-                'id' => $index+1,
-                'rule' => $rule
-            ];
-        }
-
-        return $mappedObjectRules;
-    }
-}
 
 describe('Get Ability Checker Function Test', function () {
     beforeEach(function () {
@@ -123,3 +75,134 @@ describe('Add ability test', function () {
             ->toBeTrue();
     });
 });
+
+describe("Remove the ability test", function () {
+    beforeEach(function () {
+
+        $this->storage = Mockery::mock(StorageFixture::class)->makePartial();
+        $this->abilityRepositoryInstance = new AbilityRepositoryImpl(
+            $this->storage
+        );
+
+        $this->abilityRepositoryInstance->setUserId(1);
+    });
+
+    it('cannot delete when the ability doesnt exist', function () {
+        $this->abilityRepositoryInstance->removeAbility(
+            'edit', 'resource2', 'scopeX'
+        );
+
+        $this->storage->shouldNotHaveReceived('onDeleteSpecificRule');
+    });
+
+    it('can remove the ability', function () {
+        $this->abilityRepositoryInstance->removeAbility(
+            'read', 'resource1', 'scope1'
+        );
+
+        /** @var AbilityChecker $checker */
+        $checker = $this->abilityRepositoryInstance->getChecker();
+        expect($checker->hasRule('scope1:resource1:read'))
+            ->toBeFalse();
+    });
+
+    it('can remove many abilities', function () {
+        $this->abilityRepositoryInstance->removeAbilities([
+            'scope1:resource1:read',
+            'scope1:resource2:update'
+        ]);
+
+        /** @var AbilityChecker $checker */
+        $checker = $this->abilityRepositoryInstance->getChecker();
+        expect($checker->hasRule('scope1:resource1:read'))
+            ->toBeFalse()
+            ->and($checker->hasRule('scope1:resource2:update'))
+            ->toBeFalse();
+    });
+});
+
+describe("Update the ability rule test", function () {
+    beforeEach(function () {
+
+        $this->storage = Mockery::mock(StorageFixture::class)->makePartial();
+        $this->abilityRepositoryInstance = new AbilityRepositoryImpl(
+            $this->storage
+        );
+
+        $this->abilityRepositoryInstance->setUserId(1);
+    });
+
+
+    it('cannot update the ability when the ability doesnt exist', function () {
+        $this->abilityRepositoryInstance->update(
+            'scopeX:resource2:edit',
+            'scopeX:resource2/5:edit'
+        );
+
+        $this->storage->shouldNotHaveReceived('onUpdateRule');
+    });
+
+    it('can update the ability', function () {
+        $this->abilityRepositoryInstance->update(
+            'scope1:resource1:read',
+            'scope1:resource1/666:read'
+        );
+
+        /** @var AbilityChecker $checker */
+        $checker = $this->abilityRepositoryInstance->getChecker();
+        expect($checker->hasRule('scope1:resource1:read'))
+            ->toBeFalse()
+            ->and($checker->hasRule('scope1:resource1/666:read'))
+            ->toBeTrue();
+    });
+});
+
+
+class StorageFixture implements StorageInterface
+{
+    private array $rules = [
+        'scope1:resource1:read',
+        'scope1:resource2:update'
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function onInsertNewRule(int|string $userIdentification, string $newRule): int
+    {
+        $this->rules[] = $newRule;
+        return count($this->rules);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onUpdateRule(int $ruleId, string $updatedRule): void
+    {
+        $this->rules[$ruleId - 1] = $updatedRule;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onDeleteSpecificRule(int $deletedRuleId): void
+    {
+        unset($this->rules[$deletedRuleId-1]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function onGetRulesByUserId(int|string $userIdentification): array
+    {
+        $mappedObjectRules = [];
+        foreach ($this->rules as $index=>$rule) {
+            $mappedObjectRules[] = (object) [
+                'id' => $index+1,
+                'rule' => $rule
+            ];
+        }
+
+        return $mappedObjectRules;
+    }
+}
