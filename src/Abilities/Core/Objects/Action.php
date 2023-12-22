92eb883e444cd7d330d0e2dc0132af6a28868cb9
AlexzPurewoko <purwoko908@gmail.com>
2023-12-22 15:09:02 +0700

    add logic to prevent invalid action naming

--- src/Abilities/Core/Objects/Action.php
+++ src/Abilities/Core/Objects/Action.php
@@ -15,6 +15,12 @@ class Action
         if (empty($this->action)) {
             throw new InvalidArgumentException('Action must not be empty');
         }
+
+        if (!preg_match('/^(([a-zA-Z0-9_\-])+|([*]){1})$/', $this->action)) {
+            throw new InvalidArgumentException(
+                'Invalid action naming. Please use a combination of lowercase letter, number, dash and underscore only or a single star (*) character'
+            );
+        }
     }
 
     public function get(): string
--- tests/Unit/Core/Objects/ActionTest.php
+++ tests/Unit/Core/Objects/ActionTest.php
@@ -9,6 +9,20 @@ it('must fail when assigned with empty value', function () {
     'Action must not be empty'
 );
 
+it("must throw error when passing invalid action name", function () {
+    new Action('read!');
+})->throws(
+    InvalidArgumentException::class,
+    'Invalid action naming. Please use a combination of lowercase letter, number, dash and underscore only or a single star (*) character'
+);
+
+it("must throw error when passing more than one star action", function () {
+    new Action('**');
+})->throws(
+    InvalidArgumentException::class,
+    'Invalid action naming. Please use a combination of lowercase letter, number, dash and underscore only or a single star (*) character'
+);
+
 test("Must use star symbol when using default argument", function () {
     expect((new Action())->get())->toBeString('*');
 });
