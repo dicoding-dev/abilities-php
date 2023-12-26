83afcd90c6ae46eabc674a832ce6a4e7e0097e13
AlexzPurewoko <purwoko908@gmail.com>
2023-12-26 14:25:45 +0700

    major update of API: update to set and unset the ability

--- src/Abilities/Core/Repository/WritableAbilityRepository.php
+++ src/Abilities/Core/Repository/WritableAbilityRepository.php
@@ -10,54 +10,35 @@ use Abilities\Core\Objects\Rule;
 interface WritableAbilityRepository
 {
     /**
-     * Add new capability for current user
+     * Set the new ability for current user
      *
      * @param string $action The action for a given resource. Usually, this can be a CRUD model
-     *                       like create, read, update, delete, or you can define your own model.
-     *                       To allow all action for a given resource, you can pass star '*'.
+     *                        like create, read, update, delete, or you can define your own model.
+     *                        To allow all action for a given resource, you can pass star '*'.
      * @param string $resource The object 'resource' to be accessed. Must be specific for a given {@see $scope}.
-     *                         A 'resource' is defined by domain expert on specific {@see $scope}.
+     *                          A 'resource' is defined by domain expert on specific {@see $scope}.
      * @param string $scope The scope for given rules. Impacting on how the systems select the resource
      * @param mixed|null $field Can be an object, array, or a single string, int for defining specific area of {@see $resource}
      */
-    public function addAbility(
+    public function setAbility(
         string $action,
         string $resource,
         string $scope,
         mixed $field = null,
         bool $inverted = false
     ): void;
-
-    /**
-     * Add abilities for current user
-     *
-     * @param Rule[]|string[] $rules
-     */
-    public function addAbilities(array $rules): void;
 
     /**
      * Remove the capability for the current user.
-     * See {@see addAbility()} for other parameters information
+     * See {@see setAbility()} for other parameters information
      */
-    public function removeAbility(
+    public function unsetAbility(
         string $action,
         string $resource,
         string $scope,
         mixed $field = null,
         bool $inverted = false
     ): void;
-
-    /**
-     * Remove abilities for current user
-     *
-     * @param Rule[]|string[] $rules
-     */
-    public function removeAbilities(array $rules): void;
-
-    /**
-     * Update the {@see $old} rule into {@see $new} rule
-     */
-    public function update(Rule|string $old, Rule|string $new): void;
 
     /**
      * Commit the current changes to the storage
